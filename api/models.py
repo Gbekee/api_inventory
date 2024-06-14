5965aeb0bd3811486ff31aecc6a97b71e84b9938
Gbeke <gbekeloluwaoyemakinde@gmail.com>
2024-06-14 12:30:18 +0100

    update

--- api/models.py
+++ api/models.py
@@ -14,23 +14,3 @@ class Item(models.Model):
     suppliers=models.ManyToManyField(Supplier, related_name='items')
     def __str__(self):
         return self.name
-    
-
-
-
-
-
-# from rest_framework import serializers
-# from .models import Item, Supplier
-
-# class SupplierSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Supplier
-#         fields = '__all__'
-
-# class ItemSerializer(serializers.ModelSerializer):
-#     suppliers = SupplierSerializer(many=True, read_only=True)
-    
-#     class Meta:
-#         model = Item
-#         fields = '__all__'
