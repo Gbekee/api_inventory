284e2d3efe8180877764c45723e45ff356962eac
Gbeke <gbekeloluwaoyemakinde@gmail.com>
2024-06-15 12:39:18 +0100

    update

--- api/models.py
+++ api/models.py
@@ -2,15 +2,15 @@ from django.db import models
 from datetime import date
 class Supplier(models.Model):
     name=models.CharField(max_length=255)
-    contact=models.EmailField(unique=True, blank=False)
+    contact=models.EmailField(unique=True)
     def __str__(self):
         return self.name
     
 class Item(models.Model):
     name=models.CharField(max_length=255)
     description=models.CharField(max_length=255)
-    price=models.FloatField()
-    date_added=models.DateField(default=date.today, blank=True)
+    price=models.DecimalField(max_digits=10, decimal_places=2)
+    date_added=models.DateField(default=date.today)
     suppliers=models.ManyToManyField(Supplier, related_name='items')
     def __str__(self):
         return self.name
